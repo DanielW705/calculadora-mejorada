b4c8bd9a46172251c7d14efe216747a53446a86f
Daniel <danielmtzlpz95@gmail.com>
2021-08-09 18:19:43 -0500

    Agregar archivos de proyecto.

---
+++ Models/Calculadora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora_Mejorada.Models
+{
+    public class Calculadora
+    {
+        public double nm1 { get; set; }
+        public double nm2 { get; set; }
+        public string sel { get; set; }
+        public Calculadora()
+        {
+
+        }
+
+        public (bool mathError, string resultado) calcular()
+        {
+            if (sel == "Sum")
+            {
+                return (false, (nm1 + nm2).ToString());
+            }
+            else if (sel == "Res")
+            {
+                return (false, (nm1 - nm2).ToString());
+            }
+            else if (sel == "Mult")
+            {
+                return (false, (nm1 * nm2).ToString());
+            }
+            else
+            {
+                if (nm2 != 0)
+                {
+                    return (false, (nm1 / nm2).ToString());
+                }
+                else
+                {
+                    return (true, "Math error");
+                }
+            }
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,55 @@
+﻿const mostrar = document.getElementById("resultados");
+const boton = document.querySelector("button");
+let cont = 0;
+const getData = () => {
+    cont = localStorage.getItem("contador");
+    for (let i = 0; i < cont; i++) {
+        const div = document.createElement("div");
+        const name = `operacion ${cont}`;
+        const html = localStorage.getItem(`operacion ${i}`);
+        div.setAttribute("div-number", name)
+
+        console.log();
+    }
+}
+const setData = (datos) => {
+    const datosAsubir = localStorage.setItem("DatosEscritos", datos);
+}
+const guardarHeaders = () => {
+    let headers = new Headers()
+    headers.append("Accept", "application/json");
+    headers.append("Content-Type", "application/json");
+    return headers;
+}
+const obtenerDatos = () => {
+    return JSON.stringify({
+        nm1: Number.parseFloat(document.getElementById("num1").value),
+        nm2: Number.parseFloat(document.getElementById("num2").value),
+        sel: document.getElementById("selector").value
+    });
+}
+const crearDiv = (resultado) => {
+    const div = document.createElement("div");
+    const name = `operacion ${cont}`;
+    div.innerHTML = resultado;
+    div.setAttribute("div-number", name)
+    mostrar.appendChild(div);
+    i++;
+    localStorage.setItem("contador", cont);
+    localStorage.setItem(name, div.innerHTML);
+}
+const subirdatos = () => {
+    fetch(url, {
+        method: "POST",
+        headers: guardarHeaders(),
+        body: obtenerDatos(),
+        redirect: 'follow'
+    }).then(response => {
+        response.text().then(result => {
+            crearDiv(result);
+        });
+    });
+}
+boton.addEventListener('click', () =>
+    subirdatos(), false)
+//window.addEventListener('load', () => getData(), false)
